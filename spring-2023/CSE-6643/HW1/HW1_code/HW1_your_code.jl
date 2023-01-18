e165d43cf430672406e8be38ae03ed286c7b4e2a
Karl Hiner <karl.hiner@gmail.com>
2023-01-18 15:08:16 -0500

    Almost done w CSE-6643-HW1

--- spring-2023/CSE-6643/HW1/HW1_code/HW1_your_code.jl
+++ spring-2023/CSE-6643/HW1/HW1_code/HW1_your_code.jl
@@ -1,10 +1,21 @@
-# This function takes in a matrix A and a vector v and writes their product into the vector v
+# This function takes in a matrix A and a vector v and writes their product into the vector u
 function u_is_A_times_v!(u, A, v)
-    # Write your function here! 
+    for i in 1:size(A, 1) # ith row of A
+        u[i] = 0.0
+        for j in 1:size(A, 2) # jth column of A
+            u[i] += A[i,j] * v[j]
+        end
+    end
 end
 
 # This function takes in matrices ABC and writes B times C into the matrix A
 function A_is_B_times_C!(A, B, C)
-    # Write your function here! 
+    for B_i in 1:size(B, 1) # ith row of B
+        for C_j in 1:size(C, 2) # jth column of C
+            A[B_i,C_j] = 0.0
+            for B_j in 1:size(B, 2) # jth column of B
+                A[B_i,C_j] += B[B_i,B_j] * C[B_j,C_j]
+            end
+        end
+    end
 end
-
