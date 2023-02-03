6fe074770a31febdcabeae439b33dedfa70c24ac
Karl Hiner <karl.hiner@gmail.com>
2023-02-02 21:42:36 -0500

    More hw2

--- spring-2023/CSE-6643/HW2/HW2_code/HW2_your_code.jl
+++ spring-2023/CSE-6643/HW2/HW2_code/HW2_your_code.jl
@@ -15,10 +15,20 @@ end
 # It uses blocking into blocks of size bks
 # Make sure that your function does not allocate memory
 function add_to_A_B_times_C!(A, B, C, bks)
+    for i in range(1, size(A, 1) ÷ bks + 1)
+        for j in range(1, size(A, 2) ÷ bks + 1)
+            i_rng = (i - 1)*bks + 1:min(i*bks, size(A, 1))
+            j_rng = (j - 1)*bks + 1:min(j*bks, size(A, 2))
+            add_to_A_B_times_C!(@view(A[i_rng, j_rng]), @view(B[i_rng, :]), @view(C[:, j_rng]))
+        end
+    end
 end
 
 # Implements a recursive, cache oblivious algorithm
 # complete this skeleton
+# Obtain an algorithm that equally divides each of its input matrices into four parts
+# and then recurses on the resulting eight subproblems,
+# until one of the problems reaches a size below bks.
 function oblivious_add_to_A_B_times_C!(A, B, C, bks)
     i_size = size(A, 1)
     j_size = size(C, 2)
@@ -26,6 +36,13 @@ function oblivious_add_to_A_B_times_C!(A, B, C, bks)
 
     # If we want to further subdivide
     if min(i_size, j_size, k_size) > bks
+        for i in range(1,2)
+            for j in range(1,2)
+                i_rng = (i - 1)*(i_size ÷ 2) + 1:(i == 1 ? i_size ÷ 2 : i_size)
+                j_rng = (j - 1)*(j_size ÷ 2) + 1:(j == 1 ? j_size ÷ 2 : j_size)
+                oblivious_add_to_A_B_times_C!(@view(A[i_rng, j_rng]), @view(B[i_rng, :]), @view(C[:, j_rng]), bks)
+            end
+        end
     #If we are ready to break the recursion
     else
         add_to_A_B_times_C!(A, B, C)
