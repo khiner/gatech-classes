a3b0264ab2be25a27b0720148225f3bfc4c9af67
Karl Hiner <karl.hiner@gmail.com>
2023-09-05 12:11:57 -0400

    Add code for CSE-6740 HW1

--- fall-2023/CSE-6740/HW1/myRecommender.py
+++ fall-2023/CSE-6740/HW1/myRecommender.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+def rmse(U, V, M):
+    mask = M > 0
+    res = np.sum(((U.dot(V.T) - M) * mask) ** 2) / float(np.sum(mask))
+    return np.sqrt(res)
+
+def grad_U(U, V, M, reg_coef, with_reg):
+    mask = M > 0
+    grad = -2 * np.dot((M - np.dot(U, V.T)) * mask, V) + (2 * reg_coef * U if with_reg else 0)
+    return grad
+
+def grad_V(U, V, M, reg_coef, with_reg):
+    mask = M > 0
+    grad = -2 * np.dot(((M - np.dot(U, V.T)) * mask).T, U) + (2 * reg_coef * V if with_reg else 0)
+    return grad
 
 def my_recommender(rate_mat, lr, with_reg):
     """
@@ -11,15 +25,17 @@ def my_recommender(rate_mat, lr, with_reg):
     :return:
     """
 
-    # TODO pick hyperparams
-    max_iter = 0
-    learning_rate = 0
-    reg_coef = 0
+    # TODO adjust using cross validation
+    max_iter = 1000
+    learning_rate = 0.0004
+    reg_coef = 0.01
     n_user, n_item = rate_mat.shape[0], rate_mat.shape[1]
 
     U = np.random.rand(n_user, lr) / lr
     V = np.random.rand(n_item, lr) / lr
 
-    # TODO implement your code here
+    for i in range(max_iter):
+        U -= learning_rate * grad_U(U, V, rate_mat, learning_rate, with_reg)
+        V -= learning_rate * grad_V(U, V, rate_mat, reg_coef, with_reg)
 
     return U, V
